b5a29b3d6265946feb3b386bd912c31dd9a7cf3d
lyaciocarlia <lyass.media@gmail.com>
2023-08-11 09:49:08 +0300

    Create the splitting app interface and logic

--- CheckSplitting/ContentView.swift
+++ CheckSplitting/ContentView.swift
@@ -8,14 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var check = 0.0
+    @State private var ppl = 2
+    @State private var tipPercentage = 0
+    @FocusState private var amountIsFocused: Bool
+    
+    var totalPerPerson: Double {
+        let amountPerPerson = (check + (check/100 * Double(tipPercentage)))/Double(ppl + 2)
+        return amountPerPerson
+    }
+    
+    private var tipOption = [0, 10, 15, 20, 25]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", value: $check, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                    
+                    Picker("Number or people", selection: $ppl) {
+                        ForEach (2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                Section {
+                    Picker("TipPercentage", selection: $tipPercentage) {
+                        ForEach(tipOption, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("How much tip do you want to leave?")
+                }
+                
+                Section {
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+                
+                Button("Done") {
+                    amountIsFocused = false
+                }
+            }
+            .navigationTitle("We Split")
         }
-        .padding()
     }
 }
 
